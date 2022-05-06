2ab287f80c5742a0fba9c48c294f0195447995d1
Jocyanno <jocyannovittor@hotmail.com>
2022-05-05 23:23:00 -0300

    Stage 2

--- src/components/Widget.tsx
+++ src/components/Widget.tsx
@@ -1,18 +1,22 @@
-import { ChatTeardropDots } from "phosphor-react";
 import { Popover } from '@headlessui/react';
+import { ChatTeardropDots } from 'phosphor-react';
+import { WidgetForm } from './WidgetForm/index';
 
 export function Widget() {
     return (
-        <Popover className="absolute bottom-5 right-5"> 
-            <Popover.Panel>Hello World</Popover.Panel>
-            <Popover.Button className="bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group">
-                <ChatTeardropDots className="w-6 h-6" />
+        <Popover className='absolute bottom-4 right-4 md:bottom-8 md:right-8 flex flex-col items-end'>
+            <Popover.Panel>
+                <WidgetForm />
+            </Popover.Panel>
 
-                <span className="max-w-0 overflow-hidden hover: max-w-auto group-hover:max-w-xs transition-all duration-500 ease-linear">
-                    <span className="pl-2"></span>
-                    FeedBack
+            <Popover.Button className='bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group'>
+                <ChatTeardropDots className='w-6 h-6' />
+
+                <span className='max-w-0 overflow-hidden group-hover:max-w-xs group-focus-visible:max-w-xs transition-all duration-500 ease-linear'>
+                    <span className='pl-2'></span>
+                    Feedback
                 </span>
             </Popover.Button>
         </Popover>
-    );
+    )
 }
